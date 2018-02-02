feaf10178dc493a0576ea7e5de8b1f803cfb8135
preethipriy <34617823+preethipriy@users.noreply.github.com>
2018-02-02 10:29:08 +0000

    Create numer.c

---
+++ numer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+scanf("%c",&c);
+if(c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
